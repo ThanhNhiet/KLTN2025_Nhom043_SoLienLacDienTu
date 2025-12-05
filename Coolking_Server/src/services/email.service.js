c7dcd2069c6a24c134df6766bcdd0372a5859d0a
PhamLeThanhNhiet <nhiethiz@gmail.com>
2025-12-05 17:24:31 +0700

    fix(email): update email transporter configuration to use secure port 465

--- Coolking_Server/src/services/email.service.js
+++ Coolking_Server/src/services/email.service.js
@@ -4,15 +4,15 @@ const redisService = require('./redis.service');
 // Tạo transporter cho email
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // true for 465, false for other ports
+    port: 465,
+    secure: true, // true for 465, false for other ports
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
     },
-    tls: {
-        rejectUnauthorized: false
-    }
+    // tls: {
+    //     rejectUnauthorized: false
+    // }
 });
 
 // Tạo OTP ngẫu nhiên 6 số
